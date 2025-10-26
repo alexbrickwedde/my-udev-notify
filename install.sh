43aac411968283a46230782eb5cbd8622209c39a
alex <alex>
2025-10-26 07:17:58 +0100

    added installer

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+DIR="$(dirname $(readlink -f "$0"))"
+SEDDIR=$(sed 's|/|\\/|g' <<< $DIR)
+
+sed -e "s/\/path\/to\/my-udev-notify/$SEDDIR/" < $DIR/stuff/my-udev-notify.rules | sudo tee /etc/udev/rules.d/90-my-udev-notify.rules > /dev/null
